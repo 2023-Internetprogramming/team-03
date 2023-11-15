2ce5ae0c21282a56c09cab866915fcebaf3474b7
heeone1 <rinakim1019@gmail.com>
2023-11-16 01:42:33 +0900

    Fix: gitignore 수정

--- ott_posts/models.py
+++ ott_posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class TeamMember(models.Model):
+    position = models.CharField(max_length=50)
+    skills = models.TextField()
+    people = models.IntegerField()
+    description_OTT = models.TextField()
+
+    def __str__(self):
+        return self.name

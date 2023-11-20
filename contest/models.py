a991c1a106ee1da5ca1587b360422787736088e5
heeone1 <rinakim1019@gmail.com>
2023-11-20 16:39:18 +0900

    Feat:댓글 생성

--- contest/models.py
+++ contest/models.py
@@ -6,6 +6,13 @@ class Contest(models.Model):
     deadline = models.DateField()
     link = models.URLField()
     contest_image = models.ImageField(upload_to='contest/images/%Y/%m/%d/', blank=True)
+    contest_view_count = models.IntegerField(default=0)
+
+#댓글
+class Comment(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.contest_title
--- contest/views.py
+++ contest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Contest
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import CommentForm
 
 def contest_list(request):
     contests = Contest.objects.order_by('-pk')
@@ -18,4 +19,17 @@ def contest_list(request):
 
 def contest_detail(request, contest_id):
     contest = Contest.objects.get(id=contest_id)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.contest = contest
+            comment.save()
+    else:
+        form = CommentForm()
+    
+    contest.contest_view_count += 1
+    contest.save()
+    
     return render(request, 'contest/contest_detail.html', {'contest': contest})

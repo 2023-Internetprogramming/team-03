9f34ed0e49017c1fb91fd741aad9f6a690e8380f
heeone1 <rinakim1019@gmail.com>
2023-11-28 13:16:16 +0900

    Feat: 지도 생성

--- taxi_posts/urls.py
+++ taxi_posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('rides_delete/', views.ride_delete, name='ride_delete'),
     path('rides_update/<int:id>/', views.ride_update, name='ride_update'),
     path('ride_search/', views.ridesearchResult, name='ridesearchResult'),
-    path('map/', views.map, name='map'),
+    path('map/', views.map_view, name='map_view'),
 ]

42efd28585970ecc3dd197cd2db5b91fb4e449de
heeone1 <rinakim1019@gmail.com>
2023-12-01 14:55:39 +0900

    Feat: 전체적인 css  수정

--- study_posts/forms.py
+++ study_posts/forms.py
@@ -49,7 +49,7 @@ class StudyForm(forms.ModelForm):
         ('철학전공', '철학전공'),
     ]
     
-    study_membernum = forms.TypedChoiceField(
+    study_member = forms.TypedChoiceField(
         label='모집 인원',
         choices=[(1, '1명'), (2, '2명'), (3, '3명'), (4, '4명'), (5, '5명'), (6, '6명'), (7, '7명'), (8, '8명'), (9, '9명'), (10, '10명')],
         coerce=int
@@ -58,7 +58,7 @@ class StudyForm(forms.ModelForm):
     post_content = forms.CharField(
         widget=forms.Textarea,
         label='내용',
-        help_text='추천 스터디 지우고 다른 내용을 작성 가능',
+        help_text='추천 스터디 지우고 다른 내용 작성 가능',
     )
 
     user_grade = forms.ChoiceField(choices=GRADE_CHOICES, label='학년', initial=1, widget=forms.Select())
@@ -67,7 +67,7 @@ class StudyForm(forms.ModelForm):
     class Meta:
         model = Study
         exclude = ['author']
-        fields = ["post_title", "user_major", "user_grade", "study_type", "study_membernum", "post_content"]
+        fields = ["post_title", "user_major", "user_grade", "study_type", "study_member", "post_content"]
 
         labels = {
             'post_title' : '제목',
